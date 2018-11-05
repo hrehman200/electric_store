1d70c5c404d40a19f06827fb8edfee2ea4173ca6
hrehman200 <hrehman200@gmail.com>
2018-11-06 01:11:58 +0500

    Adjusted login page look

--- application/views/login.php
+++ application/views/login.php
@@ -16,51 +16,72 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <title>Admin Login</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="<?=base_url()?>resources/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?= base_url() ?>resources/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom fonts for this template -->
-    <link href="<?=base_url()?>resources/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="<?= base_url() ?>resources/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet"
+          type="text/css">
 
     <!-- Plugin CSS -->
-    <link href="<?=base_url()?>resources/vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+    <link href="<?= base_url() ?>resources/vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="<?=base_url()?>resources/css/sb-admin.css" rel="stylesheet">
+    <link href="<?= base_url() ?>resources/css/sb-admin.css" rel="stylesheet">
     <style>
-    h2 {color: white; }
-    p {color: red;}
+        h2 {
+            color: white;
+        }
+
+        p {
+            color: red;
+        }
     </style>
 </head>
 
 <body>
- <div class="container">
+<div class="container">
 
-      <form class="form-signin" action="<?=base_url()?>login" method="POST">
-        <h2 class="form-signin-heading">Please sign in</h2>
-        <p><?php
-            if ($error == 1) { echo "Too Many Login Attempts"; }
-            if ($error == 2) { echo "Invalid Login Credentials."; }
-        ?></p> 
-        <input type="username" name="username" class="form-control" placeholder="Username" required autofocus> 
-        <input type="password" name="password" class="form-control" placeholder="Password" required> 
-        <input class="btn btn-lg btn-primary btn-block" type="submit" value="Sign in">
-      </form>
+    <div class="row">
+        <div class="col-md-4 offset-md-4">
+            <div class="login-panel panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Please Sign In</h3>
+                </div>
+                <div class="panel-body">
+                    <form class="form-signin" action="<?= base_url() ?>login" method="POST">
+                        <fieldset>
+                            <div class="form-group">
+                                <input class="form-control" placeholder="Username" name="usernmae" type="text"
+                                       autofocus="">
+                            </div>
+                            <div class="form-group">
+                                <input class="form-control" placeholder="Password" name="password" type="password"
+                                       value="">
+                            </div>
+                            <!-- Change this to a button or input when using this as a form -->
+                            <input type="submit" class="btn btn-lg btn-success btn-block" value="Login"/>
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 
-    </div> <!-- /container -->
+</div> <!-- /container -->
 
-    <!-- Bootstrap core JavaScript -->
-    <script src="<?=base_url()?>resources/vendor/jquery/jquery.min.js"></script>
-    <script src="<?=base_url()?>resources/vendor/tether/tether.min.js"></script>
-    <script src="<?=base_url()?>resources/vendor/bootstrap/js/bootstrap.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script src="<?= base_url() ?>resources/vendor/jquery/jquery.min.js"></script>
+<script src="<?= base_url() ?>resources/vendor/tether/tether.min.js"></script>
+<script src="<?= base_url() ?>resources/vendor/bootstrap/js/bootstrap.min.js"></script>
 
-    <!-- Plugin JavaScript -->
-    <script src="<?=base_url()?>resources/vendor/jquery-easing/jquery.easing.min.js"></script>
-    <script src="<?=base_url()?>resources/vendor/chart.js/Chart.min.js"></script>
-    <script src="<?=base_url()?>resources/vendor/datatables/jquery.dataTables.js"></script>
-    <script src="<?=base_url()?>resources/vendor/datatables/dataTables.bootstrap4.js"></script>
+<!-- Plugin JavaScript -->
+<script src="<?= base_url() ?>resources/vendor/jquery-easing/jquery.easing.min.js"></script>
+<script src="<?= base_url() ?>resources/vendor/chart.js/Chart.min.js"></script>
+<script src="<?= base_url() ?>resources/vendor/datatables/jquery.dataTables.js"></script>
+<script src="<?= base_url() ?>resources/vendor/datatables/dataTables.bootstrap4.js"></script>
 
-    <!-- Custom scripts for this template -->
-    <script src="<?=base_url()?>resources/js/sb-admin.min.js"></script>
+<!-- Custom scripts for this template -->
+<script src="<?= base_url() ?>resources/js/sb-admin.min.js"></script>
 
 </body>
 
